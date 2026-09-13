39f07d1d1aec4fafb4f892f8adfd2f4d34746a9f
Albert Phu <127133346+albertphu07@users.noreply.github.com>
2026-09-12 22:53:28 -0500

    Add Robot class with basic structure and methods

---
+++ robot.java
@@ -0,0 +1,31 @@
+public class Robot
+  {
+      private double Angle;
+      private String Direction;
+
+    public Robot() {
+      Angle = 0;
+      Direction = "Resting";
+    }
+    public Robot (double Ag, String Dir)
+    {
+      Angle = Ag;
+      Direction = Dir;
+    }
+    public void forward() 
+    {
+      //Add Forward Code Later
+    }
+    public void backward()
+    {
+      //Add Backward Code Later
+    }
+    public void left()
+    {
+      //Add Left Code Later
+    }
+    public void right()
+    {
+      //Add Right Code Later
+    }
+  }
