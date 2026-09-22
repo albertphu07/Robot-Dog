932caa6382be1d6a32cb7a5b7612a67c522cab98
Albert Phu <127133346+albertphu07@users.noreply.github.com>
2026-09-22 08:32:35 -0500

    Update robot_runner.java

--- robot_runner.java
+++ robot_runner.java
@@ -3,5 +3,14 @@ public class robot_runner
     public static void main(String[] agrs)
     {
       System.out.println("Runner");
+      Robot robot = new Robot();
+      
+      robot.forward();
+      TimeUnit.SECONDS.sleep(5);
+      robot.stop();
+      robot.backward();
+      TimeUnit.SECONDS.sleep(2);
+      robot.stop();
+      
     }
   }
