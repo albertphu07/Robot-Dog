b4b05c657ec3c0eb51314e5504aa406e4c281c51
Albert Phu <127133346+albertphu07@users.noreply.github.com>
2026-09-23 08:17:17 -0500

    Update robot.java

--- robot.java
+++ robot.java
@@ -1,7 +1,36 @@
+import com.pi4j.Pi4J;
+import com.pi4j.context.Context;
+import com.pi4j.io.gpio.digital.DigitalOutput;
+import com.pi4j.io.gpio.digital.Digital State;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Robot
   {
       private double Angle;
       private String Direction;
+      private static final int[] PIN_ADDRESS = {4,27,22,23,24,25,5,6,26,13,12,21};
+
+      public static void main(String[] args) throws Interupted Exception
+    {
+      Context pi4j = Pi4J.newAutoContext();
+
+      Map<Integer, DigitalOutput> pins = new HashMap<>();
+
+      for (int address : PIN_ADDRESS)
+        {
+          DigitalOutputConfigBuilder config = DigitalOutput.newConfigBuilder(pi4j)
+            .id("pin-" + address)
+            .name("GPIO Pin" + address)
+            .address(address)
+            .shutdown(DigitalState.LOW)
+            .inital(DigitalState.LOW);
+
+          DigitalOutput digitalOutput = pi4j.create(config);
+          pins.put(address, digitalOutput);
+        }
+    }
 
     public Robot() {
       Angle = 0;
@@ -14,6 +43,7 @@ public class Robot
     }
     public void forward() 
     {
+      pins.get(27).high(); //figure out movement later on ;)
       //Add Forward Code Later
     }
     public void backward()
